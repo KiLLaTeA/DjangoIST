4e16f722517f156a7f0b8b61c6cff4e01b904b1a
Maxim Kostin <poster173524@mail.ru>
2023-10-30 18:35:45 +0300

    1

--- coolSite/coolSite/urls.py
+++ coolSite/coolSite/urls.py
@@ -2,11 +2,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from women.views import pageNotFound
+from women.views import pageBadRequest, pageForbidden
+from women.views import pageNotFound, pageServerError, redirect_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('women.urls'))
+    path('women/', include('women.urls')),
+    path('/women/cat/29', redirect_view)
 ]
 
+handler400 = pageBadRequest
+handler403 = pageForbidden
 handler404 = pageNotFound
+handler500 = pageServerError
--- coolSite/women/views.py
+++ coolSite/women/views.py
@@ -1,28 +1,58 @@
 from urllib import request
-
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponseServerError,HttpResponseForbidden
+from django.http import HttpResponseBadRequest
+from django.shortcuts import render, redirect
 
+#HttpResponseBadRequest - 400
+#HttpResponseForbidden - 403
+#HttpResponseNotFound - 404
+#HttpResponseServerError - 500
 
-def index(requests):
+data_db = [
+    {'id': 1, 'FIO': 'none', 'interesting': 'what'},
+    {'id': 2, 'FIO': 'of', 'interesting': 'me'},
+    {'id': 3, 'FIO': 'me', 'interesting': 'date'},
+]
+
+def index(request):
     if (request.GET):
         print(request.GET)
+    data = {'Title': 'Главная стр.'}
+    return render(request, 'women/index.html', context=data)
     return HttpResponse('Главная страница women')
 
 
-def categories(requests):
+def categories(request):
     return HttpResponse('<h1>Привет!</h1>')
 
 
-def categories_id(requests, catid):
+def categories_id(request, catid):
     if int(catid) > 30:
         raise Http404()
     return HttpResponse(f'<h1>Статьи по номерам {catid} </h1>')
 
 
-def categories_sl(requests, catid):
+def categories_sl(request, catid):
     return HttpResponse(f'<h1>Статьи по номерам и названию {catid} </h1>')
 
 
-def pageNotFound(requests, exception):
-    return HttpResponseNotFound(f'<h1>Ошибка! Статья не найдена!</h1>')
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Ошибка 404() - Статья не найдена!</h1>')
+
+
+def pageForbidden(request, exception):
+    return HttpResponseForbidden('<h1>Ошибка 403() - Доступ запрещён!</h1>')
+
+
+def pageBadRequest(request, exception):
+    return HttpResponseBadRequest('<h1>Ошибка 400() - Неверный запрос!</h1>')
+
+
+def pageServerError(exception):
+    return HttpResponseServerError('<h1>Ошибка 500() - Ошибка сервера!</h1>')
+
+
+def redirect_view(request):
+    responce = redirect('/women/cat')
+    return responce
